d2aa84ee7210b00d5bd654210f4bd16f54028201
Jafet <106804873+DixonJafet@users.noreply.github.com>
2025-10-18 19:00:18 -0600

    Update chat.service.ts

--- DixonGPTWebSite/src/app/services/chat.service.ts
+++ DixonGPTWebSite/src/app/services/chat.service.ts
@@ -7,7 +7,7 @@ import { Message, Response } from '../models/message.model'; // Assuming Respons
   providedIn: 'root'
 })
 export class ChatService {
-  private apiUrl = 'http://3.17.4.210:80/generate'; // API endpoint
+  private apiUrl = 'https://3.17.4.210:443/generate'; // API endpoint
 
   private messagesSubject = new BehaviorSubject<Message[]>([
     {
