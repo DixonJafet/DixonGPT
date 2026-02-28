462f71a6ef6ba14fec9e5a72825bc6d64fdee282
Jafet <106804873+DixonJafet@users.noreply.github.com>
2026-02-28 08:35:21 -0600

    update API

--- DixonGPTWebSite/src/app/components/chat-container/chat-container.component.ts
+++ DixonGPTWebSite/src/app/components/chat-container/chat-container.component.ts
@@ -24,6 +24,7 @@ import { ChatInputComponent } from '../chat-input/chat-input.component';
           <div class="header-text">
             <h1>DixonGPT AI</h1>
             <p>Mini LLM based on Llama AI</p>
+            <p>Able from 9:00 a.m. to 3:30 p.m. (UTC -6) (CST)</p>
           </div>
         </div>
       </div>
